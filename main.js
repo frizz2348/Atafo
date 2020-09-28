aa246b70c801120bcc7a9d65c27dbd5ed3573c96
FRIZZ <frizz2348@gmail.com>
2020-09-28 15:00:33 +0100

    Done with the navbar, header, now curently working on the sections

---
+++ main.js
@@ -0,0 +1,25 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const sideBar = document.querySelector('.sidebar');
+const body = document.querySelector('body');
+
+
+
+let showMenu = false;
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        hamburger.classList.add('open');
+        sideBar.classList.add('open');
+        body.classList.add('open');
+        showMenu = true;
+
+    } else {
+        hamburger.classList.remove('open');
+        sideBar.classList.remove('open');
+        body.classList.remove('open');
+        showMenu = false;
+    }
+}
+
